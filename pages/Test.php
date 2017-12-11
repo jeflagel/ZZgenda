a3a0f11d3937add367621f8334b5595d4016a542
jeflagel <32105346+jeflagel@users.noreply.github.com>
2017-12-11 18:06:04 +0100

    bidon2

--- pages/Test.php
+++ pages/Test.php
@@ -1,7 +1,7 @@
 <?php
 class ConnectionTest extends PHPUnit_Framework_TestCase{
     public function setUp(){
-        require_once 'fonction.php';
+        //require_once 'fonction.php';
         //require_once '../assets/lang.php';
     }
 
