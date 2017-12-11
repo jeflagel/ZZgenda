49fdf252a9670d6b10ddd7446e422e7f0b7598a2
jeflagel <32105346+jeflagel@users.noreply.github.com>
2017-12-11 18:23:33 +0100

    automatisé

--- pages/Test.php
+++ pages/Test.php
@@ -7,10 +7,10 @@ class ConnectionTest extends PHPUnit_Framework_TestCase{
     public function testFile(){
       $ad = "ok" ;
       $this->assertTrue(1==1);
-      //$this->assertTrue(lecture(OpenFile('../assets/db/database.txt'),"jeflagel","coucou",$ad)==1 );
-      //$this->assertFalse(lecture(OpenFile('../assets/db/database.txt'),"jeflagel","aurevoir",$ad)==1 );
+      $this->assertTrue(lecture(OpenFile('../assets/db/database.txt'),"jeflagel","coucou",$ad)==1 );
+      $this->assertFalse(lecture(OpenFile('../assets/db/database.txt'),"jeflagel","aurevoir",$ad)==1 );
       $pass = "Test" ;
-      //$this->assertTrue(password_verify("$pass", Chiffrement($pass)));
+      $this->assertTrue(password_verify("$pass", Chiffrement($pass)));
 
     }
 
