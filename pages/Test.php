4c15babc58e7035e1909fbb6b071c37096c66629
jeflagel <32105346+jeflagel@users.noreply.github.com>
2017-12-11 17:52:28 +0100

    travis 5.6 deuxième essai
    ca avance

--- pages/Test.php
+++ pages/Test.php
@@ -2,10 +2,12 @@
 class ConnectionTest extends PHPUnit_Framework_TestCase{
     public function setUp(){
         require_once 'fonction.php';
+        require_once 'lang.php';
     }
 
     public function testFile(){
       $ad = "ok" ;
+      $this->assertTrue(1==1);
       $this->assertTrue(lecture(OpenFile('../assets/db/database.txt'),"jeflagel","coucou",$ad)==1 );
       $this->assertFalse(lecture(OpenFile('../assets/db/database.txt'),"jeflagel","aurevoir",$ad)==1 );
       $pass = "Test" ;
