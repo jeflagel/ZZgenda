cbb7e5337332a34e58ad775d29a6173e433a669b
jeflagel <32105346+jeflagel@users.noreply.github.com>
2017-12-12 17:07:04 +0100

    4

--- pages/Test.php
+++ pages/Test.php
@@ -7,8 +7,8 @@ class ConnectionTest extends PHPUnit_Framework_TestCase{
     public function testFile(){
       $ad = "ok" ;
       $this->assertTrue(1==1);
-      $this->assertTrue(lecture(OpenFile('../assets/db/database.txt'),"jeflagel","coucou",$ad)==1 );
-      $this->assertFalse(lecture(OpenFile('../assets/db/database.txt'),"jeflagel","aurevoir",$ad)==1 );
+      $this->assertTrue(lecture(OpenFile('/home/travis/build/jeflagel/ZZgenda/assets/db/database.txt'),"jeflagel","coucou",$ad)==1 );
+      $this->assertFalse(lecture(OpenFile('/home/travis/build/jeflagel/ZZgenda/assets/db/database.txt'),"jeflagel","aurevoir",$ad)==1 );
       $pass = "Test" ;
       $this->assertTrue(password_verify("$pass", Chiffrement($pass)));
 
