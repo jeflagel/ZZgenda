7e83ff8913c12b1a83a5c38b83f6d6dd909f94fd
jeflagel <32105346+jeflagel@users.noreply.github.com>
2017-12-13 23:15:09 +0100

    cov verif

--- pages/Test.php
+++ pages/Test.php
@@ -11,7 +11,7 @@ class ConnectionTest extends PHPUnit_Framework_TestCase{
       $this->assertFalse(lecture(OpenFile('/home/travis/build/jeflagel/ZZgenda/assets/db/database.txt'),"jeflagel","aurevoir",$ad)==1 );
       $pass = "Test" ;
       $this->assertTrue(password_verify("$pass", Chiffrement($pass)));
-      $this->assertTrue(OpenFile('fail')==0);
+      $this->assertTrue(OpenFile('fail')==0); 
     }
 
     public function testlangue(){
