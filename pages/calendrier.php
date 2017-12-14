848d9167530f29dd4077b22ddd70964669554b53
jeflagel <32105346+jeflagel@users.noreply.github.com>
2017-12-14 14:34:25 +0100

    v1

--- pages/calendrier.php
+++ pages/calendrier.php
@@ -4,13 +4,35 @@ session_start();
 if (!isset($_SESSION['auth']) || !($_SESSION['auth'])){
   header('Location: deconnexion.php');
 }
-    include ('../assets/lang.php') ;
+    include '../assets/lang.php' ;
     if(isset($_GET['lang'])){
       $langage=$_GET['lang'];
     }
     else{
       $langage='en';
     }
+
+
+function displayConf(){
+  $file = fopen("conf.json ", "r");
+  if ($file) {
+    while (($line = fgets($file)) !== false) {
+        $strConf=json_decode($line);
+        //sujet, date, heure
+        echo "<tr>";
+        echo "<td>$strConf->day</td>";
+        echo "<td>$strConf->hour</td>";
+        echo "<td>$strConf->intitule</td>";
+        echo "</tr>";
+    }
+
+    fclose($file);
+  }
+  else {
+      echo "Problème dans l'ouverture du fichier conf.json";
+  }
+}
+
  ?>
 
 <!DOCTYPE html>
@@ -42,53 +64,38 @@ if (!isset($_SESSION['auth']) || !($_SESSION['auth'])){
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="https://www.isima.fr/"><?php echo $lang['calendrier']['Disconnect'][$langage]; ?></a></li>
-        <li><a href="deconnexion.php"> <?php echo $lang['calendrier']['School'][$langage]; ?></a></li>
+        <li><a href="https://www.isima.fr/">School</a></li>
+        <li><a href="deconnexion.php"> disconnect</a></li>
       </ul>
     </div>
   </div>
 </nav>
 
 <div class="authentification">
-  <p>Hey <?php echo $_SESSION['login'] ?> : <?php echo $lang['calendrier']['look'][$langage]; ?> </p>
+  <p>Hey <?php echo $_SESSION['login'] ?> : Let's have a look </p>
 </div>
 
 <div class="container">
- <h2>Condensed Table</h2>
-  <p>The .table-condensed class makes a table more compact by cutting cell padding in half:</p>
+ <h2>Calendrier des conférences</h2>
   <table class="table table-condensed">
     <thead>
       <tr>
 
-        <th><?php echo $lang['calendrier']['firstname'][$langage]; ?></th>
-        <th><?php echo $lang['calendrier']['lastname'][$langage]; ?></th>
+        <th>Date</th>
+        <th>Heure</th>
+        <th>Conférence</th>
 
-        <th>Email</th>
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-      </tr>
+      <?php displayConf(); ?>
     </tbody>
   </table>
 </div>
 
 <!-- Footer -->
 <footer class="container-fluid bg-4 text-center">
-  <p> <img src="..\assets\img\logIsima.png" alt="Logo Isima" id="foot-img">ZZgenda by JF and AD <a href="https://github.com/jeflagel">compte github</a></p>
+  <p> <img src="..\assets\img\logIsima.png" alt="Logo Isima" id="foot-img">ZZgenda by JF <a href="https://github.com/jeflagel">compte github</a></p>
    <a href="calendrier.php?lang=fr">FR</a> <a href="calendrier.php?lang=en">EN</a>
 </footer>
 </body>
--- pages/deconnexion.php
+++ pages/deconnexion.php
@@ -1,21 +1,21 @@
 <?php
 
-    // start session
+    // On démarre la session
     session_start();
 
-    if (isset($_SESSION['auth'])) { // if connected close and disconnect
+    if (isset($_SESSION['auth'])) { // Si tu es connecté on te déconnecte et on te redirige vers une page.
 
-        // Supress session's values
+        // Supression des variables de session et de la session
         $_SESSION = array();
         session_destroy();
         session_unset ();
 
-        // Supress cookie
+        // Supression des cookies de connexion automatique
         //setcookie('login');
 
         header('Location: identification.php?deco=1');
 
-    }else{ 
+    }else{ // Dans le cas contraire on t'empêche d'accéder à cette page en te redirigeant vers la page que tu veux.
 
         header('Location: identification.php');
 
