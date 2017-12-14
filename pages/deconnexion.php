73c73792fa448e74564ab5bd3bdf5c8ca63f25bc
jeflagel <32105346+jeflagel@users.noreply.github.com>
2017-12-14 14:22:35 +0100

    remise a zero

--- pages/deconnexion.php
+++ pages/deconnexion.php
@@ -1,21 +1,21 @@
 <?php
 
-    // On démarre la session
+    // start session
     session_start();
 
-    if (isset($_SESSION['auth'])) { // Si tu es connecté on te déconnecte et on te redirige vers une page.
+    if (isset($_SESSION['auth'])) { // if connected close and disconnect
 
-        // Supression des variables de session et de la session
+        // Supress session's values
         $_SESSION = array();
         session_destroy();
         session_unset ();
 
-        // Supression des cookies de connexion automatique
+        // Supress cookie
         //setcookie('login');
 
         header('Location: identification.php?deco=1');
 
-    }else{ // Dans le cas contraire on t'empêche d'accéder à cette page en te redirigeant vers la page que tu veux.
+    }else{ 
 
         header('Location: identification.php');
 
